9925d4817fd23e11bd10e4fb97e86ec3c455655b
Julian Krzefski <krzefski@heptacom.de>
2016-12-13 22:41:21 +0100

    Added WhatsApp functionallity.

--- Views/frontend/_public/src/js/jquery.advanced-share.js
+++ Views/frontend/_public/src/js/jquery.advanced-share.js
@@ -2,15 +2,32 @@
 	'use strict';
 
 	$.plugin('heptacomAdvancedShare', {
+        url: document.URL,
+
 		init: function() {
+            var me = this;
+
 			$('.advanced-share--toggle-btn').click(function(event) {
                 event.preventDefault();
+
 				$('.advanced-share--outer-wrap').toggleClass('active');
                 $('html, body').animate({scrollTop: 0}, 400);
 			});
 
             $('.csbuttons').cSButtons();
-		}
+            me.registerWhatsapp();
+		},
+
+		registerWhatsapp: function() {
+            var me = this;
+
+            $('.advanced-share--icon-container [data-type="whatsapp"]').on('click tap', function(event) {
+                event.preventDefault();
+
+                var shareUrl = 'whatsapp://send?text=' + me.url;
+                window.open(shareUrl, '_top');
+            });
+        }
 	});
 
 	window.StateManager.addPlugin('.advanced-share--outer-wrap', 'heptacomAdvancedShare');
