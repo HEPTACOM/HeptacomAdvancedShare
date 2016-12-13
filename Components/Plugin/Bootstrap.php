19025382572489bb2bfc2ae2119a316b9a358bf3
Julian Krzefski <krzefski@heptacom.de>
2016-12-14 00:57:47 +0100

    Added getVersion()-method to bootstrap.

--- Components/Plugin/Bootstrap.php
+++ Components/Plugin/Bootstrap.php
@@ -45,6 +45,14 @@ abstract class Shopware_Plugins_Frontend_HeptacomAdvancedShare_Components_Plugin
     }
 
     /**
+     * @return mixed|null
+     */
+    public function getVersion()
+    {
+        return $this->getPluginInfo('currentVersion');
+    }
+
+    /**
      * @return array
      */
     public function getInfo()
