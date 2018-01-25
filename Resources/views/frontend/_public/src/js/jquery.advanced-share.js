c9fd4aef456b84c235005fb7311cb389eb415444
Julian Krzefski <krzefski@ksk-agentur.de>
2018-01-25 09:57:56 +0100

    Restructured event registration to support ajax reloading for variants

--- Resources/views/frontend/_public/src/js/jquery.advanced-share.js
+++ Resources/views/frontend/_public/src/js/jquery.advanced-share.js
@@ -8,6 +8,27 @@
             var me = this;
 
             window.Overlay.prototype.defaults.renderElement = '.advanced-share--inner-wrap';
+            me.registerEvents();
+        },
+
+        registerEvents: function () {
+            var me = this;
+
+            me.registerToggleButton();
+            $.subscribe('plugin/swAjaxVariant/onRequestData', function () {
+                $('.advanced-share--toggle-btn').off();
+                me.registerToggleButton();
+            });
+
+            me.registerCloseArea();
+
+            $('.csbuttons').cSButtons();
+            me.registerWhatsapp();
+            me.registerEmail();
+        },
+
+        registerToggleButton: function () {
+            var me = this;
 
             $('.advanced-share--toggle-btn').on('click tap', function (event) {
                 event.preventDefault();
@@ -15,14 +36,14 @@
                 $('.advanced-share--outer-wrap').toggleClass('advanced-share--open');
                 $('html, body').animate({scrollTop: 0}, 400);
             });
+        },
+
+        registerCloseArea: function () {
+            var me = this;
 
             $('.advanced-share--close-area').on('click tap', function () {
                 $('.advanced-share--outer-wrap').removeClass('advanced-share--open');
             });
-
-            $('.csbuttons').cSButtons();
-            me.registerWhatsapp();
-            me.registerEmail();
         },
 
         registerWhatsapp: function () {
@@ -45,9 +66,5 @@
     });
 
     window.StateManager.addPlugin('.advanced-share--outer-wrap', 'heptacomAdvancedShare');
-
-    $.subscribe('plugin/swAjaxVariant/onRequestData', function () {
-        window.StateManager.addPlugin('.advanced-share--outer-wrap', 'heptacomAdvancedShare');
-    });
 
 })(jQuery, window);
